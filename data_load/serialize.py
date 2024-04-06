0798a3b3a2eeeffdcec6b17557d082435ed11422
busiqiao <busiqiao666@gmail.com>
2024-04-06 17:57:59 +0800

    修改数据预处理函数

--- data_load/serialize.py
+++ data_load/serialize.py
@@ -1,4 +1,5 @@
 import glob
+import os
 import platform
 import multiprocessing
 from data_load.read_mat import read_eeg_mat, read_locs_xlsx
@@ -7,8 +8,9 @@ import numpy as np
 from preprocessing.aep import azim_proj, gen_images
 import einops
 
+
 def save_data(args):
-    s, f, pkl_path, locs = args  # Add locs to the arguments
+    filename, f, pkl_path, locs = args  # Add locs to the arguments
     eeg, y1, y2 = read_eeg_mat(f)  # [n_samples=5184, t_length=32, channels=124]
 
     # -----------------
@@ -31,14 +33,17 @@ def save_data(args):
         data.append(imgs[i])
         labels.append((y1[i], y2[i]))
 
-    # Save data and labels into one file
-    with open(pkl_path + f'S{s+1}.pkl', 'wb') as file:
+    # Save data and labels into one mat_file
+    with open(pkl_path + f'{filename}.pkl', 'wb') as file:
         pickle.dump((data, labels), file)
-        print(f'Saved S{s+1}.pkl')
+        print(f'Saved {filename}.pkl')
+
 
 def go_through(filenames, pkl_path, locs):  # Add locs to the arguments
+    file_names = [os.path.splitext(os.path.basename(f))[0] for f in filenames]
     with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
-        pool.map(save_data, [(s, f, pkl_path, locs) for s, f in enumerate(filenames)])  # Pass locs to save_data
+        pool.map(save_data, [(name, f, pkl_path, locs) for name, f in zip(file_names, filenames)])
+
 
 def file_scanf(path, endswith, sub_ratio=1):
     files = glob.glob(path + '/*')
@@ -51,7 +56,7 @@ def file_scanf(path, endswith, sub_ratio=1):
 
 if __name__ == "__main__":
     data_path = 'H:/EEG/EEGDATA/EEG72/'
-    filename = file_scanf(data_path, endswith='.mat')
+    mat_file = file_scanf(data_path, endswith='.mat')
     loc = read_locs_xlsx('H:/EEG/EEG-ConvTransformer/data_load/electrodes_locations/GSN_HydroCel_128.xlsx')
 
-    go_through(filename, pkl_path='H:/EEG/EEGDATA/test/', locs=loc)
+    go_through(mat_file, pkl_path='H:/EEG/EEGDATA/test/', locs=loc)
